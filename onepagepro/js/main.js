59ea84cdc791493d08cbc7dc28efe99d2d4504d6
Danis López Naranjo <kambolox@MacBook-Pro-de-Danis.local>
2018-10-29 12:38:09 -0400

    Tropix template

---
+++ onepagepro/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#boton01").click(function(){
+        $("#parrafo").addClass("corporate-menu-item");
+    });
+
+    $("#boton04").click(function(){
+        $("#parrafo").removeClass("corporate-menu-item");
+    });
+});
